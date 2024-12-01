56b565b40655db91b48c8bcc4648e83c106eee18
Rishabh Srivastava <rishabhsrivastava918@gmail.com>
2024-12-01 20:08:01 +0530

    splash screen

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+ 
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate to the next screen after a delay
+    Future.delayed(const Duration(seconds: 20), () {
+      // Replace with your navigation logic
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const NextScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/logobdc.svg', // Path to your SVG file
+                width: 100,
+                height: 100,
+                // Optional: Tint color
+              ),
+              const SizedBox(height: 20),
+              Text(
+                  'BDCOE',
+                  style: GoogleFonts.aBeeZee(
+          textStyle: const TextStyle(
+            color: Color.fromARGB(255, 245, 242, 246),
+            letterSpacing: .5,
+            fontSize: 35,
+            fontWeight: FontWeight.w500,
+          ),
+                  ),
+            ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Next Screen')),
+      body: const Center(child: Text('This is the next screen.')),
+    );
+  }
+}
